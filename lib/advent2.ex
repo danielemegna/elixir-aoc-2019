4f38dca5337d168bbfd74f22bf788d2a5af2eb7e
Daniele Megna <megna.dany@gmail.com>
2020-04-21 14:41:08 +0200

    Refactor Advent2 with right names from second part

--- lib/advent2.ex
+++ lib/advent2.ex
@@ -1,34 +1,51 @@
 defmodule Advent2 do
 
-  def resolve do
+  def resolve_first_part do
+    read_initial_memory_from_file()
+      |> put_inputs_in_memory(12, 2)
+      |> run_memory_program()
+  end
+
+  def run_memory_program(memory, instruction_pointer \\ 0) do
+    instruction = Enum.slice(memory, instruction_pointer, 4)
+    if(halt_program_instruction?(instruction)) do
+      memory
+    else
+      memory
+        |> compute_instruction(instruction)
+        |> run_memory_program(instruction_pointer + 4)
+    end
+  end
+
+  defp read_initial_memory_from_file do
     File.stream!("advent2.txt")
       |> Enum.at(0)
       |> String.split(",")
       |> Enum.map(&Integer.parse/1)
       |> Enum.map(fn({n, _}) -> n end)
-      |> List.replace_at(1, 12)
-      |> List.replace_at(2, 2)
-      |> resolve
   end
 
-  def resolve(vector, index \\ 0) do
-    if(Enum.at(vector, index) == 99) do
-      vector
-    else
-      slice = Enum.slice(vector, index, 4)
-      vector = compute_slice(slice, vector)
-      resolve(vector, index + 4)
+  defp put_inputs_in_memory(memory, noun, verb) do
+    memory
+      |> List.replace_at(1, noun)
+      |> List.replace_at(2, verb)
+  end
+
+  defp halt_program_instruction?(instruction) do
+    case(instruction) do
+      [99 | _] -> true
+      _ -> false
     end
   end
 
-  defp compute_slice([operation, first_position, second_position, result_position], full_vector) do
-    first = Enum.at(full_vector, first_position)
-    second = Enum.at(full_vector, second_position)
-    operation_result = case(operation) do
-      1 -> first + second
-      2 -> first * second
+  defp compute_instruction(memory, [opcode, first_parameter_position, second_parameter_position, result_position]) do
+    first_parameter = Enum.at(memory, first_parameter_position)
+    second_parameter = Enum.at(memory, second_parameter_position)
+    instruction_result = case(opcode) do
+      1 -> first_parameter + second_parameter
+      2 -> first_parameter * second_parameter
     end
-    List.replace_at(full_vector, result_position, operation_result)
+    List.replace_at(memory, result_position, instruction_result)
   end
 
 end
--- test/advent2_test.exs
+++ test/advent2_test.exs
@@ -1,16 +1,16 @@
 defmodule Advent2Test do
   use ExUnit.Case
 
-  test "resolve function test" do
-    assert Advent2.resolve([1,0,0,0,99]) == [2,0,0,0,99] # (1 + 1 = 2)
-    assert Advent2.resolve([2,3,0,3,99]) == [2,3,0,6,99] # (3 * 2 = 6)
-    assert Advent2.resolve([2,4,4,5,99,0]) == [2,4,4,5,99,9801] # 99 * 99 = 9801
-    assert Advent2.resolve([1,9,10,3,2,3,11,0,99,30,40,50]) == [3500,9,10,70,2,3,11,0,99,30,40,50]
-    assert Advent2.resolve([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
+  test "run_memory_program function test" do
+    assert Advent2.run_memory_program([1,0,0,0,99]) == [2,0,0,0,99] # (1 + 1 = 2)
+    assert Advent2.run_memory_program([2,3,0,3,99]) == [2,3,0,6,99] # (3 * 2 = 6)
+    assert Advent2.run_memory_program([2,4,4,5,99,0]) == [2,4,4,5,99,9801] # 99 * 99 = 9801
+    assert Advent2.run_memory_program([1,9,10,3,2,3,11,0,99,30,40,50]) == [3500,9,10,70,2,3,11,0,99,30,40,50]
+    assert Advent2.run_memory_program([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
   end
 
   test "resolve level (first part)" do
-    result = Advent2.resolve
+    result = Advent2.resolve_first_part
     assert Enum.at(result, 0) == 2894520
   end
 
