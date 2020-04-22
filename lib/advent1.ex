d445a936da098938c07ec87efdb5dd6225b50105
Daniele Megna <megna.dany@gmail.com>
2020-04-22 14:26:57 +0200

    Refactor Advent1 first part solution

--- lib/advent1.ex
+++ lib/advent1.ex
@@ -1,20 +1,23 @@
 defmodule Advent1 do
 
-  def resolve() do
-    File.stream!("advent1.txt")
-      |> Enum.map(&Integer.parse/1)
-      |> Enum.map(fn({n, _}) -> n end)
-      |> Enum.map(&calc/1)
+  def resolve_first_part() do
+    read_modules_masses_from_file()
+      |> Enum.map(&calculate_fuel_for/1)
       |> Enum.sum
   end
 
-  def calc(x) do
-    x
-      |> Decimal.new()
+  def calculate_fuel_for(mass) do
+    mass
       |> Decimal.div(3)
       |> Decimal.round(0, :down) 
       |> Decimal.to_integer
       |> Kernel.-(2)
+  end
+
+  defp read_modules_masses_from_file do
+    File.stream!("advent1.txt")
+      |> Enum.map(&Integer.parse/1)
+      |> Enum.map(fn({n, _}) -> n end)
   end
 
 end
--- test/advent1_test.exs
+++ test/advent1_test.exs
@@ -1,15 +1,15 @@
 defmodule Advent1Test do
   use ExUnit.Case
 
-  test "calc test" do
-    assert Advent1.calc(12) == 2
-    assert Advent1.calc(14) == 2
-    assert Advent1.calc(1969) == 654
-    assert Advent1.calc(100756) == 33583
+  test "calculate_fuel_for function test" do
+    assert Advent1.calculate_fuel_for(12) == 2
+    assert Advent1.calculate_fuel_for(14) == 2
+    assert Advent1.calculate_fuel_for(1969) == 654
+    assert Advent1.calculate_fuel_for(100756) == 33583
   end
 
-  test "resolve level" do
-    assert Advent1.resolve == 3262358
+  test "resolve level (first part)" do
+    assert Advent1.resolve_first_part == 3262358
   end
 
 end
