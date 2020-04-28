28f3b63201f18af75b9d05a43611c6abbcc22912
Daniele Megna <megna.dany@gmail.com>
2020-04-28 21:19:32 +0200

    First ugly and slow solutions to Advent4 (first part)

--- lib/advent4.ex
+++ lib/advent4.ex
@@ -8,15 +8,26 @@ defmodule Advent4 do
   end
 
   def meets_criteria?(password) do
-    has_two_adjacent_digits?(password) && digits_never_decrease?(password)
+    password_digits = String.graphemes(password)
+    has_two_adjacent_digits?(password_digits) &&
+      digits_never_decrease?(password_digits)
   end
 
-  defp has_two_adjacent_digits?(password) do
-    true
+  defp has_two_adjacent_digits?(password_digits) do
+    password_digits
+      |> Stream.with_index
+      |> Enum.any?(fn {digit, index} ->
+        digit == Enum.at(password_digits, index+1)
+      end)
   end
 
-  defp digits_never_decrease?(password) do
-    true
+  defp digits_never_decrease?(password_digits) do
+    password_digits
+      |> Stream.with_index
+      |> Enum.all?(fn {digit, index} ->
+        next_digit = Enum.at(password_digits, index+1)
+        index == (Enum.count(password_digits)-1) || Integer.parse(digit) <= Integer.parse(next_digit)
+      end)
   end
 
 end
--- test/advent4_test.exs
+++ test/advent4_test.exs
@@ -2,14 +2,13 @@ defmodule Advent4Test do
   use ExUnit.Case
 
   test "password meets_criteria? test" do
-    assert true == Advent4.meets_criteria?(111111)
-    assert false == Advent4.meets_criteria?(223450)
-    assert false == Advent4.meets_criteria?(123789)
+    assert true == Advent4.meets_criteria?("111111")
+    assert false == Advent4.meets_criteria?("223450")
+    assert false == Advent4.meets_criteria?("123789")
   end
 
-  @tag :skip
   test "resolve level (first part)" do
-    assert 19 == Advent4.resolve
+    assert 1640 == Advent4.resolve
   end
 
 
