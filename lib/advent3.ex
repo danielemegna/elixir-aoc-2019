9d329429ad8b6d96e9c8b6455f7a0c7cdacbda76
Daniele Megna <megna.dany@gmail.com>
2020-04-28 09:16:35 +0200

    More refactoring in Advent3

--- lib/advent3.ex
+++ lib/advent3.ex
@@ -12,10 +12,10 @@ defmodule Advent3 do
 
   def find_closest_cross_manhattan_distance(first_wire_movements, second_wire_movements) do
     first_grid_coordinates = %{}
-      |> fill_grid_with(first_wire_movements, {0,0}, 0)
+      |> fill_grid_with(first_wire_movements)
       |> Map.keys
     second_grid_coordinates = %{}
-      |> fill_grid_with(second_wire_movements, {0,0}, 0)
+      |> fill_grid_with(second_wire_movements)
       |> Map.keys
     
     MapSet.intersection(MapSet.new(first_grid_coordinates), MapSet.new(second_grid_coordinates))
@@ -24,8 +24,8 @@ defmodule Advent3 do
   end
 
   def find_fewest_combined_steps_to_cross(first_wire_movements, second_wire_movements) do
-    first_grid = %{} |> fill_grid_with(first_wire_movements, {0,0}, 0)
-    second_grid = %{} |> fill_grid_with(second_wire_movements, {0,0}, 0)
+    first_grid = %{} |> fill_grid_with(first_wire_movements)
+    second_grid = %{} |> fill_grid_with(second_wire_movements)
 
     first_grid
       |> Enum.filter(fn {coordinate, _} -> Map.has_key?(second_grid, coordinate) end)
@@ -36,6 +36,7 @@ defmodule Advent3 do
       |> Enum.min
   end
 
+  def fill_grid_with(grid, movements), do: fill_grid_with(grid, movements, {0,0}, 0)
   def fill_grid_with(grid, [movement | rest], current_coordinate, current_wire_length) do
     {direction, steps_number} = String.split_at(movement, 1)
     {steps_number, _} = Integer.parse(steps_number)
@@ -43,14 +44,14 @@ defmodule Advent3 do
     new_grid = 1..steps_number
       |> Enum.reduce(grid, fn step_index, acc ->
         step_coordinate = increase_coordinate(current_coordinate, direction, step_index)
-        Map.put(acc, step_coordinate, current_wire_length + step_index)
+        step_wire_length = current_wire_length + step_index
+        Map.put(acc, step_coordinate, step_wire_length)
       end)
-
     new_coordinate = increase_coordinate(current_coordinate, direction, steps_number)
+    new_wire_length = current_wire_length + steps_number
 
-    fill_grid_with(new_grid, rest, new_coordinate, current_wire_length + steps_number)
+    fill_grid_with(new_grid, rest, new_coordinate, new_wire_length)
   end
-
   def fill_grid_with(grid, [], _, _), do: grid
 
   defp increase_coordinate({x, y}, "R", count), do: {x + count, y}
