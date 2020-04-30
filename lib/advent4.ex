c7122cab79659c17c53c154f4da602ed63e130af
Daniele Megna <megna.dany@gmail.com>
2020-04-30 08:53:39 +0200

    Very faaaster Advent4 solution with recursion

--- lib/advent4.ex
+++ lib/advent4.ex
@@ -2,36 +2,32 @@ defmodule Advent4 do
 
   def resolve do
     199999..669999 # 197487..673251
-      |> Enum.map(&Integer.to_string/1)
       |> Enum.filter(&meets_criteria?/1)
       |> Enum.count
   end
 
   def meets_criteria?(password) do
-    password_digits = String.graphemes(password)
+    password_digits = Integer.digits(password)
     has_two_adjacent_digits?(password_digits) &&
       digits_never_decrease?(password_digits)
   end
 
-  defp has_two_adjacent_digits?(password_digits) do
-    password_digits
-      |> Enum.with_index
-      |> Enum.reverse
-      |> Enum.drop(1)
-      |> Enum.any?(fn {digit, index} ->
-        digit == Enum.at(password_digits, index+1)
-      end)
+  defp has_two_adjacent_digits?([first, second | rest]) do
+    if(first == second) do
+      true
+    else
+      has_two_adjacent_digits?([second | rest])
+    end
   end
+  defp has_two_adjacent_digits?(_), do: false
 
-  defp digits_never_decrease?(password_digits) do
-    password_digits
-      |> Enum.with_index
-      |> Enum.reverse
-      |> Enum.drop(1)
-      |> Enum.all?(fn {digit, index} ->
-        next_digit = Enum.at(password_digits, index+1)
-        Integer.parse(digit) <= Integer.parse(next_digit)
-      end)
+  defp digits_never_decrease?([first, second | rest]) do
+    if(first > second) do
+      false
+    else
+      digits_never_decrease?([second | rest])
+    end
   end
+  defp digits_never_decrease?(_), do: true
 
 end
--- test/advent4_test.exs
+++ test/advent4_test.exs
@@ -2,9 +2,11 @@ defmodule Advent4Test do
   use ExUnit.Case
 
   test "password meets_criteria? test" do
-    assert true == Advent4.meets_criteria?("111111")
-    assert false == Advent4.meets_criteria?("223450")
-    assert false == Advent4.meets_criteria?("123789")
+    assert true == Advent4.meets_criteria?(111111)
+    assert true == Advent4.meets_criteria?(122456)
+    assert false == Advent4.meets_criteria?(223450)
+    assert false == Advent4.meets_criteria?(123789)
+    assert false == Advent4.meets_criteria?(113734)
   end
 
   test "resolve level (first part)" do
