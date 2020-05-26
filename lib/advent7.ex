c3f04381bb8fe4569edf235e86b4f06bd678016d
Daniele Megna <megna.dany@gmail.com>
2020-05-26 03:27:55 +0200

    Developed Advent7.max_thruster_signal_for(memory)

--- lib/advent7.ex
+++ lib/advent7.ex
@@ -1,15 +1,33 @@
 defmodule Advent7 do
 
-def max_thruster_signal_for(memory) do
-  run_amplifier_controller_software_with(memory, [4,3,2,1,0], 0)
-end
+  def max_thruster_signal_for(memory) do
+    get_permutations([0,1,2,3,4])
+      |> Enum.map(fn(phase_settings_sequence) ->
+        run_amplifier_controller_software_with(memory, phase_settings_sequence, 0)
+      end)
+      |> Enum.max
+  end
 
-defp run_amplifier_controller_software_with(_, [], input_signal), do: input_signal
 
-defp run_amplifier_controller_software_with(memory, [phase_setting | rest_phase], input_signal) do
-  {_, outputs} = Advent5.run_memory_program(memory, [phase_setting, input_signal])
-  run_amplifier_controller_software_with(memory, rest_phase, Enum.at(outputs,0))
-end
+  defp run_amplifier_controller_software_with(memory, [phase_setting | rest_phase], input_signal) do
+    {_final_memory, outputs} = Advent5.run_memory_program(memory, [phase_setting, input_signal])
+    output_signal = Enum.at(outputs, 0)
 
+    if(Enum.empty?(rest_phase)) do
+      output_signal
+    else
+      run_amplifier_controller_software_with(memory, rest_phase, output_signal)
+    end
+  end
+
+  defp get_permutations([single]), do: [[single]]
+  defp get_permutations(list) do
+    list |> Enum.flat_map(fn(element) ->
+      rest = list -- [element]
+      rest_permutations = get_permutations(rest)
+      rest_permutations
+        |> Enum.map(fn(storter_permutation) -> [element | storter_permutation] end)
+    end)
+  end
 
 end
