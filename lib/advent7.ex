3b757211a0b2ec8605dd688c47c60784d56e57cf
Daniele Megna <megna.dany@gmail.com>
2020-05-30 00:39:09 +0200

    More refactoring in Advent7
    merge max_thruster_signal_for and
    max_thruster_signal_with_feedback_loop_for extracting
    phase_settings_sequence_elements as second function argument

--- lib/advent7.ex
+++ lib/advent7.ex
@@ -1,23 +1,15 @@
 defmodule Advent7 do
 
   def resolve_first_part do
-    max_thruster_signal_for(read_initial_memory_from_file())
-  end
-
-  def max_thruster_signal_for(memory) do
-    get_permutations([0,1,2,3,4])
-      |> Enum.map(fn(phase_settings_sequence) ->
-        thruster_signal_with(memory, phase_settings_sequence)
-      end)
-      |> Enum.max
+    max_thruster_signal_for(read_initial_memory_from_file(), [0,1,2,3,4])
   end
 
   def resolve_second_part do
-    max_thruster_signal_with_feedback_loop_for(read_initial_memory_from_file())
+    max_thruster_signal_for(read_initial_memory_from_file(), [5,6,7,8,9])
   end
 
-  def max_thruster_signal_with_feedback_loop_for(memory) do
-    get_permutations([5,6,7,8,9])
+  def max_thruster_signal_for(memory, phase_settings_sequence_elements) do
+    generate_permutations(phase_settings_sequence_elements)
       |> Enum.map(fn(phase_settings_sequence) ->
         thruster_signal_with(memory, phase_settings_sequence)
       end)
@@ -34,9 +26,9 @@ defmodule Advent7 do
     inputs = [phase_setting, input_signal] |> Enum.filter(&(&1 !== nil))
 
     {memory, memory_pointer} = amplifier_states |> Enum.at(amplifier_index)
-    {new_memory, last_instruction, outputs} = Advent5.run_memory_program_from_instruction(
-      memory, memory_pointer, inputs, []
-    )
+    {new_memory, last_instruction, outputs} =
+      Advent5.run_memory_program_from_instruction(memory, memory_pointer, inputs, [])
+
     output_signal = Enum.at(outputs, 0)
 
     if(last_instruction.code.opcode === :halt && amplifier_index === 4) do
@@ -45,16 +37,17 @@ defmodule Advent7 do
       amplifier_states = amplifier_states
         |> List.replace_at(amplifier_index, {new_memory, last_instruction.memory_pointer})
 
-      run_amplifier_software_with(amplifier_states, phase_settings_sequence_stack, rem(amplifier_index+1, 5), output_signal)
+      next_amplifier_index = rem(amplifier_index+1, 5)
+      run_amplifier_software_with(amplifier_states, phase_settings_sequence_stack, next_amplifier_index, output_signal)
     end
 
   end
 
-  defp get_permutations([single]), do: [[single]]
-  defp get_permutations(list) do
+  defp generate_permutations([single]), do: [[single]]
+  defp generate_permutations(list) do
     list |> Enum.flat_map(fn(element) ->
       rest = list -- [element]
-      rest_permutations = get_permutations(rest)
+      rest_permutations = generate_permutations(rest)
       rest_permutations
         |> Enum.map(fn(storter_permutation) -> [element | storter_permutation] end) 
     end)
